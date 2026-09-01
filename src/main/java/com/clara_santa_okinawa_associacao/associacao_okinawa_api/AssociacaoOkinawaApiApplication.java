3af2e75f0e701cd0bd74dc0b2973542384702a87
NicollasAlmeidaLopes14 <nicollas.lopes@sptech.school>
2026-09-01 18:04:45 -0300

    chore: inicia a estrutura do projeto

---
+++ src/main/java/com/clara_santa_okinawa_associacao/associacao_okinawa_api/AssociacaoOkinawaApiApplication.java
@@ -0,0 +1,13 @@
+package com.clara_santa_okinawa_associacao.associacao_okinawa_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssociacaoOkinawaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssociacaoOkinawaApiApplication.class, args);
+	}
+
+}
